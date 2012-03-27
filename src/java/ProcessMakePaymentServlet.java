ed3a27fb3a319142649513f65b5143d727105232
unknown <astiat.2009@Asti-Windows.(none)>
2012-03-28 01:19:44 +0800

    Modified to use log4j.

--- src/java/ProcessMakePaymentServlet.java
+++ src/java/ProcessMakePaymentServlet.java
@@ -1,5 +1,7 @@
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,15 +9,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.*;
 import java.util.concurrent.Future;
 
+import org.apache.log4j.Logger;
+
 import test.DbBean;
 
 public class ProcessMakePaymentServlet extends HttpServlet {
 
+    static Logger log = Logger.getLogger(ProcessMakePaymentServlet.class);
+
+    private long startTime;
     private final ExecutorService executor = Executors.newFixedThreadPool(1);
 
-    // Study this method carefully
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        startTimer();
 
         // initialize parameters for invoking remote method
         final String bankId = request.getParameter("bankId");
@@ -31,6 +39,7 @@ public class ProcessMakePaymentServlet extends HttpServlet {
 
         //submit a new executor to inform payment gateway
         Future<Boolean> future = executor.submit(new Callable() {
+
             public Boolean call() {
                 boolean result = false;
 
@@ -43,7 +52,8 @@ public class ProcessMakePaymentServlet extends HttpServlet {
                     result = port.pay(bankId, bankPwd, payeeId, referenceId, amt);
 
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    log.error("Exception caught when using payment gateway, printing stacktrace...");
+                    log.error(e);
                 }
                 return result;
             }
@@ -65,15 +75,20 @@ public class ProcessMakePaymentServlet extends HttpServlet {
 
             //wait for result from the executor
             successfullyInformedPaymentGateway = future.get();
+            
+            startTime = stopTimer();
 
             if (successfulDebit && successfullyInformedPaymentGateway) {
-                System.out.println("Payment to gateway was successful.");
+                out.println("Payment to gateway was successful.");
+                log.info("Payment to gateway was successful.");
                 recordPaymentMade(userId, amt, referenceId);
             } else if (!successfullyInformedPaymentGateway) {
-                out.println("Payment to gateway failed, crediting amt back.");
+                out.println("Payment to gateway failed, crediting amount back to account.");
+                log.info("Payment to gateway failed, crediting amount back to account.");
                 creditAccount(userId, amt);
             } else if (!successfulDebit) {
                 out.println("Unable to debit from your account.");
+                log.info("Unable to debit from your account.");
             }
 
             if (successfulDebit) {
@@ -99,6 +114,7 @@ public class ProcessMakePaymentServlet extends HttpServlet {
         out.println("<br/><br/>");
         out.println("Click <a href='loginSuccessUser.jsp'>here</a> to return to menu<br/>");
         out.println("Click <a href='makePayment.jsp'>here</a> to make another payment<br/>");
+        out.println("Took " + String.valueOf(startTime) + "ms");
         out.println("</body></html>");
         out.close();
     }
@@ -133,6 +149,16 @@ public class ProcessMakePaymentServlet extends HttpServlet {
 
         boolean result = db.credit(userId, amt);
         return result;
+    }
+
+    // sets startTime to current clock time
+    private void startTimer() {
+        startTime = new Date().getTime();
+    }
+
+    // returns no. of mseconds elapsed since startTimer() was called
+    private long stopTimer() {
+        return (new Date().getTime()) - startTime;
     }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
 
